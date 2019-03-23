1079276e74471d480a6186581c59f04ffd4058fd
Hentioe_Cl <me@bluerain.io>
2019-03-23 21:34:08 +0800

    Update module name

--- spec/cicr_spec.cr
+++ spec/cicr_spec.cr
@@ -1,6 +1,6 @@
 require "./spec_helper"
 
-describe Cicr do
+describe CICR do
   # TODO: Write tests
 
   it "works" do
--- src/cicr.cr
+++ src/cicr.cr
@@ -1,5 +1,5 @@
-# TODO: Write documentation for `Cicr`
-module Cicr
+# TODO: Write documentation for `CICR`
+module CICR
   VERSION = "0.1.0-dev"
 
   # TODO: Put your code here
